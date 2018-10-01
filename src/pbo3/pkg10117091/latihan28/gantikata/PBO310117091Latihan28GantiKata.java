22362f86eabd0ea8e65dd5b7d388ee583981bb74
Dida Handian <handiandida@gmail.com>
2018-10-01 20:40:42 +0700

    GantiKata

---
+++ src/pbo3/pkg10117091/latihan28/gantikata/PBO310117091Latihan28GantiKata.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan28.gantikata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ * Nama         : Dida Handian
+ * Kelas        : IF-3 
+ * Nim          : 10117091 
+ * Deskripsi    : Program ini dapat mengganti kata dalam sebuah kalimat dimana 
+ * user menginputkan sendiri kalimatnya
+ */
+public class PBO310117091Latihan28GantiKata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String kalimatAwal;
+        String kalimatBaru;
+        String gantiKata;
+        String jadiKata;
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("====Program Ganti Kata====\n");
+        System.out.print("Masukkan Kalimat : ");
+        kalimatAwal = scn.nextLine();
+        System.out.print("Ganti Kata : ");
+        gantiKata = scn.next();
+        System.out.print("Menjadi Kata : ");
+        jadiKata = scn.next();
+        
+        kalimatBaru =  (kalimatAwal).replaceAll(gantiKata, jadiKata);
+        
+        System.out.println("\n====Hasil Formatting====");
+        System.out.println("Kalimat Awal : "+kalimatAwal);
+        System.out.println("Kalimat Baru : "+kalimatBaru);
+    }
+    
+}
